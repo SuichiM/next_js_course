7425fdd76d0cbef1683ea9ebd317632defa830ce
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-11-25 17:43:15 -0300

    using Next request&response

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,13 +1,21 @@
-import {IncomingMessage, ServerResponse} from 'http'
+import {NextApiRequest, NextApiResponse} from 'next'
 import DB from '@database'
 
-const OneAvo = async (req:IncomingMessage, res:ServerResponse ) =>{
+const OneAvo = async (req:NextApiRequest, res:NextApiResponse ) =>{
   const db = new DB()
   const avoID = req.query.id
-  const avo = await db.getById(avoID)
+  const avo = await db.getById(avoID as string)
+  
+  res.status(200).json({data:avo})
+
+  /* 
+  la linea anterior es equivalente a todo esto 
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({data: avo}))
+ */
+
 }
 
 export default OneAvo
