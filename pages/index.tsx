ba08eb17240d31614e68ed49b4ab4a872b426049
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-11-28 19:03:50 -0300

    printed product data on product page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Link from 'next/link'
 
 const HomePage = () => {
   const [productList, setProductList] = useState<TProduct[]>([])
 
-  useEffect(()=>{
-    window.fetch('api/avo')
-    .then(response => response.json())
-    .then(({data, lenght}) => setProductList(data))
-    .catch(err=>console.log(err))
+  useEffect(() => {
+    window
+      .fetch('api/avo')
+      .then((response) => response.json())
+      .then(({ data, lenght }) => setProductList(data))
+      .catch((err) => console.log(err))
   }, [])
 
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
-      {productList.map(product => 
-        <div>{product.name}</div>  
-      )}
+      <ul>
+        {productList.map((product) => (
+          <li>
+            <Link href={`/product/${product.id}`}>
+              {product.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
+  const {id} = query 
 
+  const [avo, setAvo] = useState<TProduct>({})
+
+  useEffect(()=>{
+    window.fetch(`../api/avo/${id}`)
+    .then(response => response.json())
+    .then(({data}) => setAvo(data))
+    .catch(err=>console.log(err))
+  }, [id])
+
+  
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>{avo.name} <small style={{fontSize:'small'}}> SKU:{avo.sku}</small></h1>
+      <p>
+        {avo.attributes && avo.attributes.description}
+      </p>
     </section>
   )
 }
