b11950d347690e2e2cac7dd94da36681fb1d9074
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-12-03 16:42:56 -0300

    again

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const URL = process.env.BKND_URL 
+ 
 
 export const getServerSideProps = async ()=>{
-
+  // const URL = process.env.BKND_URL
+  const URL = process.env.BKND_URL || 'https://avo-suichim-store.vercel.app'
+  console.log('main URL:'+URL)
   const response = await fetch(`${URL}/api/avo`)
-
+  
   const {data:productList}:TAPIAvoResponse = await response.json()
   return{
     props:{
