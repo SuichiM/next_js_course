b908f8332d7e9e92c4b7daaee294fae8f8e5a3c3
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-12-03 22:01:19 -0300

    typo

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -15,7 +15,7 @@ export const getStaticPaths = async () => {
       "User-Agent":
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
       Accept: "application/json; charset=UTF-8",
-    },
+    }}
   )
   const { data: productList }: TAPIAvoResponse = await response.json()
   const paths = productList.map(({ id }) => ({ params: { id } }))
