9213eeb2ae9b79c2482b541841b5ae352b2a88f3
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-12-03 21:58:06 -0300

    fixes build

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -7,7 +7,16 @@ const PROTOCOL = process.env.PROTOCOL
 const URL = process.env.BKND_URL
 
 export const getStaticPaths = async () => {
-  const response = await fetch(`${PROTOCOL}${URL}/api/avo`)
+  const response = await fetch(`${PROTOCOL}${URL}/api/avo`,
+  {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+      Accept: "application/json; charset=UTF-8",
+    },
+  )
   const { data: productList }: TAPIAvoResponse = await response.json()
   const paths = productList.map(({ id }) => ({ params: { id } }))
   return {
